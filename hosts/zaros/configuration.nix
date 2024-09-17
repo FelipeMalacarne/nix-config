612729b1c6f2d5ef1b14cfb41ffdd9f3a0c1b508
FelipeMalacarne <felipemalacarne012@gmail.com>
2024-09-17 01:19:20 -0300

    allow unfree

--- hosts/zaros/configuration.nix
+++ hosts/zaros/configuration.nix
@@ -90,6 +90,8 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
