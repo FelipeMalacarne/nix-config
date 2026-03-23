02f1b435d0285bccf0d934b3564ec7dfd130b2b7
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-03-23 19:27:05 -0300

    feat: add flake.nix with all inputs

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Felipe's system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # noctalia-qs must be top-level so follows overrides apply correctly
+    # and only one nixpkgs version ends up in the closure
+    noctalia-qs = {
+      url = "github:noctalia-dev/noctalia-qs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.noctalia-qs.follows = "noctalia-qs";
+    };
+
+    nvim-config.url = "github:FelipeMalacarne/nvim";
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }@inputs:
+    {
+      nixosConfigurations = {
+        zaros = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/zaros
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.extraSpecialArgs = { inherit inputs; };
+            }
+          ];
+        };
+      };
+    };
+}
