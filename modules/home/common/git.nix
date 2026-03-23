0f45c8a2ca640e5b9dd46078741e3bc214aaebd0
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-03-23 20:19:26 -0300

    fix: use programs.git.signing.format to silence deprecation warning

--- modules/home/common/git.nix
+++ modules/home/common/git.nix
@@ -3,12 +3,12 @@
 {
   programs.git = {
     enable = true;
+    signing.format = null; # adopt new default, silence deprecation warning
     settings = {
       user.name = "FelipeMalacarne";
       user.email = "felipemalacarne012@gmail.com";
       init.defaultBranch = "main";
       pull.rebase = true;
-      gpg.format = null; # silence signing.format deprecation warning
     };
   };
 }
