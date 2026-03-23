35ee8740a77fa35fbe00ca65cc13dcd3bcfcc678
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-03-23 19:29:39 -0300

    feat: add nixos core module (locale, nix settings)

---
+++ modules/nixos/core.nix
@@ -0,0 +1,22 @@
+# modules/nixos/core.nix
+{ inputs, pkgs, ... }:
+{
+  # Locale and timezone
+  i18n.defaultLocale = "pt_BR.UTF-8";
+  time.timeZone = "America/Sao_Paulo";
+
+  # Nix settings
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    auto-optimise-store = true;
+  };
+
+  # Pin the flake registry to the same nixpkgs used by this flake
+  # so `nix run nixpkgs#foo` uses the same version as the system
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
+
+  nixpkgs.config.allowUnfree = true;
+}
