d60acdac1004fd081bb67ef273e03043ec2e560b
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-04-12 10:54:54 -0300

    feat(nixos): add kdeconnect module with firewall rules
    Enable KDE Connect and configure firewall port ranges for both TCP and
    UDP. Integrate with home-manager to enable the service and package for
    users when available.

---
+++ modules/nixos/kdeconnect.nix
@@ -0,0 +1,19 @@
+{ lib, options, ... }:
+{
+  programs.kdeconnect.enable = true;
+
+  networking.firewall = rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
+
+  home-manager.sharedModules = lib.optional (options ? home-manager) ({ pkgs, ... }: {
+    services.kdeconnect.enable = true;
+    home.packages = [ pkgs.kdePackages.kdeconnect-kde ];
+  });
+}
