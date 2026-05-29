0f0ca4dc8ec36f658fd2dd53c953d776c61e183f
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-05-29 17:07:13 -0300

    chore(nvim): comment out eslint_d and neo-tree options
    Commented out eslint_d linters for JavaScript/React in
    diagnostics.nvim-lint
    to temporarily disable them. Also commented out several neo-tree setup
    options, including file watcher and filtered items, possibly for
    troubleshooting or to revert to defaults. No functional changes, just
    configuration adjustments for easier testing and maintenance.

--- modules/features/nvim/config/formatting.nix
+++ modules/features/nvim/config/formatting.nix
@@ -29,9 +29,9 @@
     diagnostics.nvim-lint = {
       enable = true;
       linters_by_ft = {
-        javascript = [ "eslint_d" ];
-        javascriptreact = [ "eslint_d" ];
-        typescriptreact = [ "eslint_d" ];
+        # javascript = [ "eslint_d" ];
+        # javascriptreact = [ "eslint_d" ];
+        # typescriptreact = [ "eslint_d" ];
       };
     };
   };
--- modules/features/nvim/config/plugins/neo-tree.nix
+++ modules/features/nvim/config/plugins/neo-tree.nix
@@ -3,23 +3,23 @@
   vim.filetree.neo-tree = {
     enable = true;
     setupOpts = {
-      close_if_last_window = true;
-      sources = [ "filesystem" ];
-      filesystem = {
-        bind_to_cwd = false;
-        follow_current_file.enabled = true;
-        use_libuv_file_watcher = true;
-        filtered_items = {
-          visible = false;
-          hide_dotfiles = false;
-          hide_gitignored = false;
-          hide_by_name = [
-            ".git"
-            "node_modules"
-            "vendor"
-          ];
-        };
-      };
+      # close_if_last_window = true;
+      # sources = [ "filesystem" ];
+      # filesystem = {
+      #   bind_to_cwd = false;
+      #   follow_current_file.enabled = true;
+      #   use_libuv_file_watcher = true;
+      #   filtered_items = {
+      #     visible = false;
+      #     hide_dotfiles = false;
+      #     hide_gitignored = true;
+      #     hide_by_name = [
+      #       ".git"
+      #       "node_modules"
+      #       "vendor"
+      #     ];
+      #   };
+      # };
       window = {
         position = "float";
         width = 35;
