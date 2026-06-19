789cc53c2d1982e5eb472cd6197a51da81dd33e7
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-06-18 22:56:36 -0300

    Fix: webos-dev-manager

--- modules/features/webos-dev-manager.nix
+++ modules/features/webos-dev-manager.nix
@@ -13,16 +13,31 @@
           name = "${pname}-${version}.AppImage";
           hash = "sha256-1Eg8flL81vJXcGG9492tePqI4LpvEap2spuYtfIwAKU=";
         };
+
+        extraPkgs = pkgs: with pkgs; [ mesa libGL ];
+      };
+
+      devManager = pkgs.writeShellApplication {
+        name = "webos-dev-manager";
+        runtimeInputs = [ devManagerAppImage ];
+        text = ''
+          export LD_LIBRARY_PATH="${pkgs.mesa}/lib:${pkgs.libGL}/lib:/run/opengl-driver/lib''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+          export WEBKIT_DISABLE_COMPOSITING_MODE=1
+          export GDK_BACKEND=x11
+          export GBM_BACKEND=nvidia-drm
+          export __EGL_VENDOR_LIBRARY_FILENAMES="${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json"
+          exec ${devManagerAppImage}/bin/webos-dev-manager "$@"
+        '';
       };
     in
     {
       home-manager.users.${user} = {
-        home.packages = [ devManagerAppImage ];
+        home.packages = [ devManager ];
 
         xdg.desktopEntries.webos-dev-manager = {
           name = "WebOS Dev Manager";
           genericName = "webOS TV Developer Tools";
-          exec = "${devManagerAppImage}/bin/webos-dev-manager";
+          exec = "${devManager}/bin/webos-dev-manager";
           terminal = false;
           categories = [ "Development" "Utility" ];
         };
