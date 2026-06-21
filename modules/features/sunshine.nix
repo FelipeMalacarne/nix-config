45b40c545d70ac37a41590e3cb915d785d054437
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-06-21 20:14:23 -0300

    fix: commenting sunshine config

--- modules/features/sunshine.nix
+++ modules/features/sunshine.nix
@@ -20,19 +20,19 @@
           }
         );
 
-        settings = {
-          locale = "pt_BR";
-          sunshine_name = "zaros";
-          encoder = "nvenc";
-          capture = "wlr";
-          max_bitrate = "0";
-          hevc_mode = "1";
-          nvenc_preset = "p4";
-          nvenc_twopass = "disabled";
-          nvenc_spatial_aq = "enabled";
-          fec_percentage = "5";
-          port = 47989;
-        };
+        # settings = {
+        #   locale = "pt_BR";
+        #   sunshine_name = "zaros";
+        #   encoder = "nvenc";
+        #   capture = "wlr";
+        #   max_bitrate = "0";
+        #   hevc_mode = "1";
+        #   nvenc_preset = "p4";
+        #   nvenc_twopass = "disabled";
+        #   nvenc_spatial_aq = "enabled";
+        #   fec_percentage = "5";
+        #   port = 47989;
+        # };
       };
 
       users.users.${config.my.user.name}.extraGroups = [ "uinput" ];
