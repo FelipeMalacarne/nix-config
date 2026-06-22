71cde28c491af452a5c72fd97e067dbb44ce8f24
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-06-22 10:45:16 -0300

    feat: rest of options

--- modules/packages/neovim/config/options.lua
+++ modules/packages/neovim/config/options.lua
@@ -32,3 +32,56 @@ opt.ignorecase = true
 opt.smartcase = true
 opt.hlsearch = true
 opt.incsearch = true
+
+-- Completion
+opt.completeopt = "menuone,noinsert,noselect"
+
+-- Files / undo
+local undodir = vim.fn.expand("~/.vim/undodir")
+if vim.fn.isdirectory(undodir) == 0 then
+	vim.fn.mkdir(undodir, "p")
+end
+opt.backup = false
+opt.writebackup = false
+opt.swapfile = false
+opt.undofile = true
+opt.undodir = undodir
+opt.autoread = true
+opt.autowrite = false
+
+-- Timing
+opt.updatetime = 300
+opt.timeoutlen = 500
+opt.ttimeoutlen = 0
+
+-- Behaviour
+opt.hidden = true
+opt.errorbells = false
+opt.backspace = "indent,eol,start"
+opt.autochdir = false
+opt.iskeyword:append("-")
+opt.path:append("**")
+opt.selection = "inclusive"
+opt.mouse = "a"
+opt.clipboard:append("unnamedplus")
+opt.modifiable = true
+opt.encoding = "UTF-8"
+
+opt.guicursor =
+	"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+
+-- Folding (treesitter-based)
+opt.foldmethod = "expr"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.foldlevel = 99
+
+-- Splits
+opt.splitbelow = true
+opt.splitright = true
+
+-- Wildmenu / misc
+opt.wildmenu = true
+opt.wildmode = "longest:full,full"
+opt.diffopt:append("linematch:60")
+opt.redrawtime = 10000
+opt.maxmempattern = 20000
