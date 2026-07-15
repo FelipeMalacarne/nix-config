32d4dcaf382fd84954a7fa72a3baac97327487e8
FelipeMalacarne <felipemalacarne012@gmail.com>
2026-07-15 19:55:29 -0300

    docker can emulate

--- modules/features/docker.nix
+++ modules/features/docker.nix
@@ -5,6 +5,10 @@
       user = config.my.user.name;
     in
     {
+      boot.binfmt = {
+        emulatedSystems = [ "aarch64-linux" ];
+        preferStaticEmulators = true;
+      };
       virtualisation.docker.enable = true;
       users.users.${user}.extraGroups = [ "docker" ];
     };
